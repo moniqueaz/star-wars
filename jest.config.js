eec403c3f24f3dd43d4fa6e216562c0ee860fc91
Monique Azevedo <monique.azevedo@b2wdigital.com>
2022-07-03 11:16:59 -0300

    chore: adiciona jest e reorganiza arquivos

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+    '!**/coverage/**',
+  ],
+  coverageReporters: ['json', 'lcov', 'text', 'clover', 'html'],
+  coverageThreshold: {
+    'src/**/**': {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.tsx'],
+  moduleDirectories: ['node_modules', 'src'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+};
